480cf5cefa5d67026d5290902aa35104e047bb4e
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-12-09 22:04:25 +0300

    Initial Commit

---
+++ TESTS/TESTSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTSApp.swift
+//  TESTS
+//
+//  Created by Marat Fakhrizhanov on 09.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TESTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
